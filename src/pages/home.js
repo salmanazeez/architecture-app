0002a81c8adb25a6569e9c054b88e30a5e3205fa
Salman Azeez <salmanazeez15@gmail.com>
2019-12-26 14:49:10 +0530

    Partially add texts

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import NavBar from '../components/navbar';
 import {Row,
         Col, 
         } from 'react-bootstrap';
+import Typography from '@material-ui/core/Typography';
 
 function Home() {
     return (
@@ -14,6 +15,22 @@ function Home() {
                     <NavBar></NavBar>
                 </Col>
             </Row>
+            <div className="contents-container">
+                <Row>
+                    <Col xs={12} md={6}>
+                        <Col xs={12} md={4}></Col>
+                        <Col xs={12} md={8}>
+                            <div className="text-container">
+                                <Typography variant="subtitle1">Modern architecture</Typography>
+                                <Typography variant="h1">Design &</Typography>
+                                <Typography variant="h1"><strong>Architecture</strong></Typography>
+                                <Typography variant="subtitle1">Found in Riga with 7 offices in Latvia, Baltic Arch is a company made up of a highly specialized team of architects, designers and engineers.</Typography>
+                            </div>
+                        </Col>
+                    </Col>
+                    <Col xs={12} md={6}></Col>
+                </Row>
+            </div>
         </div>
     );
 }
